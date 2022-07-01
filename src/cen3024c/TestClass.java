0196ca884c13ba6900d6bc7b0201c1b654172153
Stephen Sturges Jr <stephen.sturges.jr@outlook.com>
2022-07-01 19:44:37 -0400

    Rewrote Multithread method. It's is now accurate, but still slow.

--- src/cen3024c/TestClass.java
+++ src/cen3024c/TestClass.java
@@ -1,60 +1,38 @@
-/**
- * @author Stephen Sturges Jr
- * @version 06/30/2022
- */
 package cen3024c;
 
 import java.text.DecimalFormat;
 
-public class TestClass {
-	
-	private static int count = 0;
-	private static int currentIndex = 0;
-	
-	public static synchronized void sumIndexValue(int[] array, int arrayIndex) {
-		count += array[arrayIndex];
-	}
-	
-	public static synchronized void incrementIndex() {
-		currentIndex++;
-	}
-	
-	/**
-	 * The main method.
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		int n = 200000000;
-		int lowerBound = 1;
-		int upperBound = 10;
-		int[] randomArray = RandomIntegerArray.generateRandomArray(n, lowerBound, upperBound);
-		singleThreadSumTimed(randomArray);
-		
-		Thread t1 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (currentIndex < randomArray.length) {
-					sumIndexValue(randomArray, currentIndex);
-					incrementIndex();
-				}
-			}
-		});
-		
-		Thread t2 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (currentIndex < randomArray.length) {
-					sumIndexValue(randomArray, currentIndex);
-					incrementIndex();
-				}
-			}
-		});
+public class TestClass implements Runnable {
 
+	static int sizeOfArray = 200000000;
+	static int lowerBound = 1;
+	static int upperBound = 10;
+	static int[] randomArray = RandomIntegerArray.generateRandomArray(sizeOfArray, lowerBound, upperBound);
+	static int arrayIndex = 0;
+	static int count = 0;
+	static long elapsedTime = 0;
+	
+	public static synchronized void incrementArrayIndex() {
+		arrayIndex++;
+	} // End of incrementArrayIndex method.
+	
+	public static synchronized void countArray() {
+		count = count + randomArray[arrayIndex];
+	} // End of countArray method.
+	
+	public static void main(String[] args) {
+		SingleThreadCount.singleThreadSumTimed(randomArray);
+		Thread t1 = new Thread(new TestClass());
 		long startTime = System.nanoTime();
 		t1.start();
-		t2.start();
 		try {
 			t1.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Thread t2 = new Thread(new TestClass());
+		t2.start();
+		try {
 			t2.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -63,37 +41,14 @@ public class TestClass {
 		long elapsedTime = endTime - startTime;
 		DecimalFormat df = new DecimalFormat("###,###,###");
 		System.out.println("Multi-thread:\n\tElapsed time = " + df.format(elapsedTime) + " ns\n\tSum = " + df.format(count) + " (Possible range: 200,000,000 - 2,000,000,000)");
-	} // End of main.
-	
-	/**
-	 * Times the execution of the sumArray method.
-	 * @param array
-	 */
-	public static void singleThreadSumTimed(int[] array) {
-		long startTime = System.nanoTime();
-		int result = sumArray(array);
-		long endTime = System.nanoTime();
-		long elapsedTime = endTime - startTime;
-		
-		// TODO Scorekeeping:
-//		long elapsedTimeMax;
-//		long elapsedTimeMin;
-		
-		DecimalFormat df = new DecimalFormat("###,###,###");
-		System.out.println("Single-thread:\n\tElapsed time = " + df.format(elapsedTime) + " ns\n\tSum = " + df.format(result) + " (Possible range: 200,000,000 - 2,000,000,000)");
-	} // End of singleThreadSumTimed.
-	
-	/**
-	 * Sums the elements of an integer array.
-	 * @param array
-	 * @return
-	 */
-	public static int sumArray(int[] array) {
-		int result = 0;
-		for (int i = 0; i < array.length; i++) {
-			result += array[i];
-		} // End of for loop.
-		return result;
-	} // End of sumArray.
+	} // End of main method.
 
-} // End of TestClass.
+	@Override
+	public void run() {
+		while(arrayIndex < sizeOfArray) {
+			countArray();
+			incrementArrayIndex();
+		} // End of while loop.
+	} // End of run method.
+
+} // End of TestClass class.
