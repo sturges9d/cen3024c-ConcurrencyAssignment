9a179efdf727fa36b8c9c9fbad6e7a165f2c3706
Stephen Sturges Jr <stephen.sturges.jr@outlook.com>
2022-07-01 18:09:55 -0400

    Implemented multithread count, but it is inaccurate and slow compared to the single-thread count.

--- src/cen3024c/TestClass.java
+++ src/cen3024c/TestClass.java
@@ -8,6 +8,17 @@ import java.text.DecimalFormat;
 
 public class TestClass {
 	
+	private static int count = 0;
+	private static int currentIndex = 0;
+	
+	public static synchronized void sumIndexValue(int[] array, int arrayIndex) {
+		count += array[arrayIndex];
+	}
+	
+	public static synchronized void incrementIndex() {
+		currentIndex++;
+	}
+	
 	/**
 	 * The main method.
 	 * @param args
@@ -18,8 +29,40 @@ public class TestClass {
 		int upperBound = 10;
 		int[] randomArray = RandomIntegerArray.generateRandomArray(n, lowerBound, upperBound);
 		singleThreadSumTimed(randomArray);
-		Thread t1 = new Thread(new MultiThreadCount(randomArray, 0));
+		
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (currentIndex < randomArray.length) {
+					sumIndexValue(randomArray, currentIndex);
+					incrementIndex();
+				}
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (currentIndex < randomArray.length) {
+					sumIndexValue(randomArray, currentIndex);
+					incrementIndex();
+				}
+			}
+		});
+
+		long startTime = System.nanoTime();
 		t1.start();
+		t2.start();
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		long endTime = System.nanoTime();
+		long elapsedTime = endTime - startTime;
+		DecimalFormat df = new DecimalFormat("###,###,###");
+		System.out.println("Multi-thread:\n\tElapsed time = " + df.format(elapsedTime) + " ns\n\tSum = " + df.format(count) + " (Possible range: 200,000,000 - 2,000,000,000)");
 	} // End of main.
 	
 	/**
@@ -36,9 +79,8 @@ public class TestClass {
 //		long elapsedTimeMax;
 //		long elapsedTimeMin;
 		
-		
 		DecimalFormat df = new DecimalFormat("###,###,###");
-		System.out.println("Single thread:\n\tElapsed time = " + df.format(elapsedTime) + " ns\n\tSum = " + df.format(result) + " (Range: 200,000,000 - 2,000,000,000)");
+		System.out.println("Single-thread:\n\tElapsed time = " + df.format(elapsedTime) + " ns\n\tSum = " + df.format(result) + " (Possible range: 200,000,000 - 2,000,000,000)");
 	} // End of singleThreadSumTimed.
 	
 	/**
